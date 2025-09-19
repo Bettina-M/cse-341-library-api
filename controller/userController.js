e66bb1a4cf44aee6eb6a70bc145fe8a5c36a2857
Bettina-M <men23034@byui.edu>
2025-09-19 11:48:32 +0300

    more user routes

--- controller/userController.js
+++ controller/userController.js
@@ -18,4 +18,41 @@ async function registerUser(req, res, next){
     
 }
 
-module.exports = {registerUser}
+async function getAllUsers(req, res, next){
+    try{
+        const users = await User.find()
+        if(!users) return res.status(404).json({message:'No users found'})
+        res.status(200).json(users)
+    }catch(error){
+        next(error)
+    }
+}
+
+async function updateUser(req, res, next){
+    try{
+        const userId = req.params.id
+        const updateData ={
+            userName: req.body.userName,
+            email: req.body.email,
+            password: req.body.password
+        }
+        const user = await User.findByIdAndUpdate(userId, updateData, {new:true})
+        if(!user) return res.status(404).json({message:'User not found'})
+        res.status(200).json(user)
+    }catch(error){
+        next(error)
+    }   
+}
+
+async function deleteUser(req, res, next){
+    try{
+        const userId = req.params.id
+        const user = await User.findByIdAndDelete(userId)
+        if(!user) return res.status(404).json({message:'User not found'})
+        res.status(200).json({message:'User deleted successfully'})
+    }catch(error){
+        next(error)
+    }   
+}
+
+module.exports = {registerUser, getAllUsers, updateUser, deleteUser}
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/',validator.userValidationCheck, validator.validateUser, controller.registerUser)
-
+router.get('/', controller.getAllUsers)
+router.put('/:id', validator.userValidationCheck, validator.validateUser, controller.updateUser)
+router.delete('/:id', controller.deleteUser)
 module.exports = router
