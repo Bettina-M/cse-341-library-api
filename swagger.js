8a16e26011897bf1e90893364cccdb68b4ea5b1b
Bettina-M <men23034@byui.edu>
2025-09-25 15:56:28 +0300

    OAuth Fix

--- swagger.js
+++ swagger.js
@@ -2,32 +2,39 @@ const swaggerAutogen = require('swagger-autogen')({ openapi: '3.0.0' })
 
 
 const doc = {
-    info: {
-        title: 'Library API',
-        description: 'API documentation for library service',
+  info: {
+    title: 'Library API',
+    description: 'API documentation for library service',
+  },
+  servers: [
+    {
+      url: 'https://cse-341-library-api-fpy9.onrender.com',
+      description: 'Production server (Render deployment)'
     },
-    host: 'cse-341-library-api-fpy9.onrender.com',
-    schemes: ['https'],
-    components: {
-        securitySchemes: {
-            oauth2: {
-                type: 'oauth2',
-                flows: {
-                    authorizationCode: {
-                        authorizationUrl: "https://dev-gbwrsi3upfsfczwp.us.auth0.com/authorize",
-                        tokenUrl: "https://dev-gbwrsi3upfsfczwp.us.auth0.com/oauth/token",
-                        scopes: {
-                            "openid": "OpenID Connect scope",
-                            "profile": "User profile info",
-                            "email": "User email",
-                            "read:books": "Read access to books"
-                        }
-                    }
-                }
+    {
+      url: 'http://localhost:8080',
+      description: 'Local development server'
+    }
+  ],
+  components: {
+    securitySchemes: {
+      oauth2: {
+        type: 'oauth2',
+        flows: {
+          authorizationCode: {
+            authorizationUrl: "https://dev-gbwrsi3upfsfczwp.us.auth0.com/authorize",
+            tokenUrl: "https://dev-gbwrsi3upfsfczwp.us.auth0.com/oauth/token",
+            scopes: {
+              "openid": "OpenID Connect scope",
+              "profile": "User profile info",
+              "email": "User email",
+              "read:books": "Read access to books"
             }
+          }
         }
-    },
-    
+      }
+    }
+  }
 }
 
 const outputFile = './swagger-output.json'
